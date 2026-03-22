f50e0d85b98bff2d472f5f31f8f5a40956efcce1
chrysalis-iiserb <chrysalis@iiserb.ac.in>
2026-03-22 15:11:55 +0530

    Update MoreStories.tsx

--- components/MoreStories.tsx
+++ components/MoreStories.tsx
@@ -3,11 +3,11 @@ import type { Post } from 'lib/sanity.queries'
 
 export default function MoreStories({ posts }: { posts: Post[] }) {
   return (
-    <section>
-      <h2 className="mb-8 text-6xl font-bold leading-tight tracking-tighter md:text-7xl">
-        More Stories
+    <section className="mb-16">
+      <h2 className="mb-8 text-3xl font-bold tracking-tight border-b pb-4">
+        Browse Blogs
       </h2>
-      <div className="mb-32 grid grid-cols-1 gap-y-20 md:grid-cols-2 md:gap-x-16 md:gap-y-32 lg:gap-x-32">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {posts.map((post) => (
           <PostPreview
             key={post._id}
